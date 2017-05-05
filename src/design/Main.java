955c12961809a86950186876f8476f11121cda6c
Student 6 <Student 6@10.8.33.171>
2017-05-05 12:18:05 -0400

    adsf

--- src/design/Main.java
+++ src/design/Main.java
@@ -1,16 +1,24 @@
 package design;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application{
-	
+	Stage window;
 	public static void main(String[] args){
 		launch(args);
 	}
 
 	@Override
 	public void start(Stage stage) throws Exception {
+		window = stage;
+		window.setTitle("TD GAME");
 		
+		VBox vcont = new VBox();
+		Scene startMenu = new Scene(vcont, 1000, 700);
 		
+		window.setScene(startMenu);
+		window.show();
 	}
 }
