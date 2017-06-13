0d4e15c2e938343b88869ccee87a42b81b776ccf
Ray <Ray@192.168.1.107>
2017-06-13 13:36:08 -0400

    asfd

--- src/objects/towers/IceTower.java
+++ src/objects/towers/IceTower.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.util.Duration;
 import objects.AoeTower;
 import objects.Enemy;
 import ui.Main;
@@ -17,10 +20,19 @@ public class IceTower extends AoeTower{
 	@Override
 	public void fire(ArrayList<Enemy> enemies) {
 		if(canFire()){
-			for(Enemy e: enemies){
-				e.takeDamage(getDamage());
-				e.getTransition().setRate(0.5);
-			}
+			Circle aoe = new Circle(getX()+getFitWidth()/2, getY()+getFitHeight()/2, getRange(), Color.rgb(102, 179, 255, 0.3));
+			aoe.setStroke(Color.rgb(204, 230, 255));
+			Main.addNode(aoe);
+			new Timeline(new KeyFrame(getDelay().divide(2), ev -> {
+				Main.removeNode(aoe);
+				for(Enemy e: enemies){
+					e.takeDamage(getDamage());
+					if(e.getTransition().getCurrentRate() == 1){
+						e.getTransition().setRate(0.5);
+						new Timeline(new KeyFrame(Duration.seconds(2), af -> e.getTransition().setRate(1))).play();
+					}
+				}
+			})).play();
 			setCanFire(false);
 			new Timeline(new KeyFrame(getDelay(), ev -> setCanFire(true))).play();
 		}
